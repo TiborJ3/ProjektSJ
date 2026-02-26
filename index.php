738dbb924f2ddf9c373ff0f92625cde331de7fe3
TiborJ3 <tibor.jendreas@student.ukf.sk>
2026-02-26 18:03:06 +0100

    Prvy commit
    Prvy commit

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moja prvá stránka v PHP</title>
+</head>
+<body>
+<h1>Vitajte</h1>
 <?php
 echo "<h1>hello world</h1>";
+echo "Dnes je" .date("d/m/y"). "<br>";
+echo "Aktualny čas je " .date("H:i:s"). "<br>";
 ?>
+</body>
+</html>
